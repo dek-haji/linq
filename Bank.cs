1bbd6cf715999698999e30e57e79247fd5bbd1ef
dek-haji <deeeqhaji@gmail.com>
2019-07-15 11:25:51 -0500

    did some of the exercises.

---
+++ Bank.cs
@@ -0,0 +1,10 @@
+ using System.Collections.Generic;
+namespace linq
+{
+    public class Bank
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public int MillionaireCount { get; set; }
+    }
+}
